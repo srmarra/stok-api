b0a8bf6a8b265e25c79564690968f03150d129a8
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-01 08:00:10 -0300

    Update index.php

--- auth/registrar/index.php
+++ auth/registrar/index.php
@@ -16,11 +16,15 @@
         $stmt->execute(array('email' => $email));
 
         if($stmt->rowCount() == 0){
-            $stmt = $PDO->prepare("INSERT INTO tb_user values (null, :nome , :email , :senha");
+            $stmt = $PDO->prepare('INSERT INTO tb_user values (null, :nome , :email , :senha');
             $senha =  password_hash($dados->{'senha'}, PASSWORD_DEFAULT);
             $nome = $dados->{'nome'};
 
-            $stmt->execute(array('nome'=>$nome,'email'=>$email,'senha'=>$senha));
+            $stmt->execute(array(
+                'nome'=>$nome,
+                'email'=>$email,
+                'senha'=>$senha));
+
                 $retorno = array(
                     "status" => true,
                     "key" => 2
