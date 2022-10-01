5a573b455d22a5be8f308f8e754498cc1318b6b6
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-01 00:32:24 -0300

    Update index.php

--- auth/registrar/index.php
+++ auth/registrar/index.php
@@ -8,13 +8,26 @@
     $status = false;
     $dados = json_decode(file_get_contents("php://input"));
     if(isset($dados->{"nome"})){
-        
+        $stmt = $PDO->prepare('SELECT * FROM tb_user where user_email = :email');
+        $stmt->execute(array('email' => $dados{'email'}));
 
         $retorno = array(
             "status" => true,
             "key"=> "eihuhuehue"
         );
         echo json_encode($retorno);
+            
+            
+
+
+        }else{
+            $retorno = array(
+                "status" => false,
+                "erro" => 1
+            );
+
+            echo json_encode($retorno);
+        }
 
     }else{
         $retorno = array(
