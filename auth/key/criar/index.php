744d9360c3c92e4f9589b190f952dfba3cbc3615
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-01 08:23:29 -0300

    a

--- auth/key/criar/index.php
+++ auth/key/criar/index.php
@@ -20,7 +20,7 @@
 
     if($stmt->rowCount() == 1){
         $user = $stmt->fetch();
-        if(password_hash($senha, PASSWORD_DEFAULT) == $user['user_password']){
+        if(password_verify($senha,$user['user_password'])){
             regenerar:
             $key = uniqid("key_",true);
             $stmt = $PDO->prepare('SELECT * from tb_auth where auth_key = :key');
