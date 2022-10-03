9be9d0c5fd4b73c09697fca68272a1d18a9696c4
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-02 21:56:11 -0300

    Update index.php

--- produtos/criar/index.php
+++ produtos/criar/index.php
@@ -7,8 +7,7 @@
     $Dados_Recebidos = file_get_contents("php://input");
     $dados = json_decode($Dados_Recebidos);
     $key = $dados['titulo'];
-    $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = ".$key);
-    $smtp->execute(array(""=>""));
+    $smtp = $PDO->query("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = ".$key);
 
     if($smtp->rowCount() > 0){
         $json = array(
