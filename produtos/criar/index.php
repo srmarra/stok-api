938663ffd6231ba4df72a3bc05b482fa34066a8e
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-02 21:52:01 -0300

    Update index.php

--- produtos/criar/index.php
+++ produtos/criar/index.php
@@ -6,11 +6,9 @@
     require_once("../../util/pdo_connect.php");
     $Dados_Recebidos = file_get_contents("php://input");
     $dados = json_decode($Dados_Recebidos);
-    $key = "test";
-    $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = :key ");
-    $smtp->execute(array(
-        "key"=>$key
-    ));
+    $key = $dados['titulo'];
+    $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = $key ");
+    $smtp->execute();
 
     if($smtp->rowCount() > 0){
         $json = array(
