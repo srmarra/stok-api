5de1b43f199f8f520bbf04f3729cb48daa91823f
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-02 21:36:41 -0300

    Update index.php

--- produtos/criar/index.php
+++ produtos/criar/index.php
@@ -7,9 +7,10 @@
     $Dados_Recebidos = file_get_contents("php://input");
     $key = $Dados_Recebidos['key'];
     $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = :key ");
-    $smTp->execute(array(
+    $smtp->execute(array(
         "key"=>$key
     ));
+
 
     
     
