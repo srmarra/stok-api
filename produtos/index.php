36f4ca59561be8886e1b1379717a3ef194793652
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-02 22:58:45 -0300

    Update index.php

--- produtos/index.php
+++ produtos/index.php
@@ -1,4 +1,31 @@
 <?php
     require_once("../util/pdo_connect.php");
 
+    header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+
+    $Dados_Recebidos = file_get_contents("php://input");
+    $dados = json_decode($Dados_Recebidos);
+    $key = $dados->{'key'};
+    
+    
+
+    $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = :key ");
+    $smtp->execute(array(
+        "key"=>$key
+    ));
+
+    if($smtp->rowCount() >0){
+
+    }else{
+
+        $json = array(
+            "status"=>false,
+        );
+        echo json_encode($json);
+    }
+
+
+
     
