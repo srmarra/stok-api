022e88b1d005163daf8c249387ecea1a687a1c46
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-02 21:48:08 -0300

    Update index.php

--- produtos/criar/index.php
+++ produtos/criar/index.php
@@ -4,14 +4,15 @@
     header("Access-Control-Allow-Headers: *");
 
     require_once("../../util/pdo_connect.php");
-    $Dados_Recebidos = json_decode(file_get_contents("php://input"));
-    $key = $Dados_Recebidos['key'];
+    $Dados_Recebidos = file_get_contents("php://input");
+    $dados = json_decode($Dados_Recebidos);
+    $key = $dados['key'];
     $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = :key ");
     $smtp->execute(array(
         "key"=>$key
     ));
 
-    if($smtp->rowCount() > 0){
+    if($smtp->rowCount() > 100){
         $json = array(
             "status"=>true
         );
