7107da0e2cdaed91265b6c0f76a330c7b12b1a90
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-02 21:34:13 -0300

    Update index.php

--- produtos/criar/index.php
+++ produtos/criar/index.php
@@ -6,7 +6,7 @@
     require_once("../../util/pdo_connect.php");
     $Dados_Recebidos = file_get_contents("php://input");
     $key = $Dados_Recebidos['key'];
-    
-
+    $smtp = $PDO->prepare("SELECT A.auth_key,U.user_id FROM tb_auth A INNER JOIN tb_user U on U.user_id = A.auth_user_id WHERE A.auth_key = :key ");
+   
     
     
