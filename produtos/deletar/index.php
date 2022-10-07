bccee5c3fbf2b67ef7a3e8005d9ae8c9bdc20328
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-07 08:27:57 -0300

    Update index.php

--- produtos/deletar/index.php
+++ produtos/deletar/index.php
@@ -3,12 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
 
-    require_once("../../util/pdo_connect.php");
 
-    $Dados_Recebidos = file_get_contents("php//input");
-    $dados = json_decode($Dados_Recebidos);
-    $key = $dados->{'key'};
-    $id = $dados->{'id'};
 
     $json = array(
         "key" => $key,
