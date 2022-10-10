2af06ac410f5a4b5da3c8b3eb7bcbde3a262460c
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-09 21:12:21 -0300

    Create index.php

---
+++ produtos/editar/index.php
@@ -0,0 +1,16 @@
+<?php
+    require_once("../util/pdo_connect.php");
+
+    header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+
+    $Dados_Recebidos = file_get_contents("php://input");
+    $dados = json_decode($Dados_Recebidos);
+
+    $json = array(
+        "status" => true
+    );  
+
+    echo $json;
+
