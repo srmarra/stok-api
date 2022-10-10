41c5327de56b19c1bc39c019e0e1e20a3ad8283b
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-09 21:41:26 -0300

    Update index.php

--- produtos/editar/index.php
+++ produtos/editar/index.php
@@ -3,11 +3,16 @@
     header('Content-Type: application/json');
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
-    require_once("../../util/pdo_connect.php")
+    require_once("../../util/pdo_connect.php");
+
     $Dados_Recebidos = file_get_contents("php://input");
     $dados = json_decode($Dados_Recebidos);
 
-    
+    $json = array(
+        "status" => true,
+        "Titulo"=> $dados->{'titulo'}
+    );  
+
     
     echo json_encode($json);
 
