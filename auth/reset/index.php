da7823bff46fc06fe7c47dfefad07b0db6655303
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-13 20:16:06 -0300

    Update index.php

--- auth/reset/index.php
+++ auth/reset/index.php
@@ -1,6 +1,21 @@
 <?php
+    header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    $Dados_Recebidos = file_get_contents("php://input");
+    $dados = json_decode($Dados_Recebidos);
+
+    // 
 
     require_once("../../util/pdo_connect.php");
 
     $codigoRedefinicao = rand(1000,9999);
-    echo $codigoRedefinicao;
+
+    keyGerar:
+    $key_reset = uniqid("key_",true);
+
+
+    $json = array(
+        "key"=>$key_reset,
+        "code"=>$codigoRedefinicao
+    );
