524f047769e45a2b2823be90b06aa3b6122e807a
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-14 08:49:43 -0300

    Adicionei phpmailer

--- auth/reset/index.php
+++ auth/reset/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
     header('Content-Type: application/json');
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: *");
@@ -7,7 +10,31 @@
 
     // 
 
-    require_once("../../util/pdo_connect.php");
+    require_once("../../util/pdo_connect.php");//Conexão com banco de dados
+    require_once("../../libraries/phpmailer/src/PHPMailer.php");//Classe PHPMAILER
+
+
+    // Configuração Email
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = "smtp-relay.sendinblue.com";
+    $mail->SMTPAuth=true;
+    $mail->Username="stok@hmarra.tech";
+    $mail->Password="QjD8vzgAsmYd7Iwf";
+    // 
+
+
+    // Para
+    $mail->AddAddress("herick.marra1@gmail.com");
+    
+
+    $mail->isHTML(true);
+
+    $mail->Subject="Resetar a senha STOK";//Assunto
+    $mail->Body="Funcionou";//Corpo
+    $mail->AltBody = "Erro aoler a mensagem";
+
+    $envio = $mail->send();
 
     $codigoRedefinicao = rand(1000,9999);
 
@@ -17,7 +44,8 @@
 
     $json = array(
         "key"=>$key_reset,
-        "code"=>$codigoRedefinicao
+        "code"=>$codigoRedefinicao,
+        "status"=>$envio
     );
 
     echo json_encode($json);
