56222cc31838a06dbc23102be9e69537852dbe1f
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-14 09:05:51 -0300

    atualização

--- auth/reset/index.php
+++ auth/reset/index.php
@@ -11,31 +11,23 @@ use PHPMailer\PHPMailer\PHPMailer;
     // 
 
     require_once("../../util/pdo_connect.php");//Conexão com banco de dados
-    require_once("../../libraries/phpmailer/src/PHPMailer.php");//Classe PHPMAILER
 
 
-    // Configuração Email
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->Host = "smtp-relay.sendinblue.com";
-    $mail->SMTPAuth=true;
-    $mail->Username="stok@hmarra.tech";
-    $mail->Password="QjD8vzgAsmYd7Iwf";
-    // 
+    // Configuração do email
+    $headers  = "From: Stok <stok@hmarra.tech>\n";
+    $headers .= "X-Sender: testsite <mail@testsite.com>\n";
+    $headers .= 'X-Mailer: PHP/' . phpversion();
+    $headers .= "X-Priority: 1\n"; // Urgent message!
+    $headers .= "Return-Path: stok@hmarra.tech\n"; // Return path for errors
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\n";
 
+    $para = "herick.marra1@gmail.com";
+    $Assunto = "STOK Redefinir senha";
+    $body = "<h1>test</h1>";
 
-    // Para
-    $mail->AddAddress("herick.marra1@gmail.com");
     
-
-    $mail->isHTML(true);
-
-    $mail->Subject="Resetar a senha STOK";//Assunto
-    $mail->Body="Funcionou";//Corpo
-    $mail->AltBody = "Erro aoler a mensagem";
-
-    $envio = $mail->send();
-
+    mail($para,$Assunto,$body,$headers);
     $codigoRedefinicao = rand(1000,9999);
 
     keyGerar:
@@ -44,8 +36,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
     $json = array(
         "key"=>$key_reset,
-        "code"=>$codigoRedefinicao,
-        "status"=>$envio
+        "code"=>$codigoRedefinicao
     );
 
     echo json_encode($json);
