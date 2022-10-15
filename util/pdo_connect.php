42c9c335be94e4b93df7295f05171193d4505da7
SrMarra <110024369+srmarra@users.noreply.github.com>
2022-10-15 00:53:22 -0300

    ,

--- util/pdo_connect.php
+++ util/pdo_connect.php
@@ -1,8 +1,14 @@
 <?php
+
+    //Db config
     $user = "u337436534_stok";
     $password = '/l0X!KVl';
+    $dbname = 'u337436534_stok';
+    $host = "92.249.44.52";
+
+
     try{
-        $PDO = new PDO('mysql:host=92.249.44.52;dbname=u337436534_stok',$user,$password);
+        $PDO = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
         $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         echo 'ERROR'.$e->getMessage();
